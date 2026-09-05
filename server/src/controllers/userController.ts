710e28fee6d37474af3c485dad98fbb73c18226d
Rafiq Harhash <rafeqnusr@icloud.com>
2026-09-05 20:23:19 +0200

    fix: remove native bcrypt import

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
 import { User } from '../models/User';
-import bcrypt from 'bcrypt';
 import { sendResponse } from '../utils/responseFormatter';
 import { AppError } from '../middleware/errorHandler';
 
